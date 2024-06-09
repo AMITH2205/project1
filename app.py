2f46e7990d2ff0d24935fc22df5d57a32c8b6de6
amith2205 <amithrajrr@gmail.com>
2024-06-09 20:15:48 +0530

    chnage

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return "Hello, World! This is a Python web application running in Docker."
 
+
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
